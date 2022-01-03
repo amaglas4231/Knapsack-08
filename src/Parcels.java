2082def061964846fdef199cbf0f53dc43ca79e6
Lea H <88035738+inconnu1567@users.noreply.github.com>
2022-01-03 12:28:54 +0100

    Add files via upload

---
+++ src/Parcels.java
@@ -0,0 +1,73 @@
+package Draft;
+
+public class Parcels{
+    
+    private String name;
+    private int rotation;
+    private int colors;
+    private double value;
+    private int placed;
+    private int amount;
+
+    Parcels(String n, int r, int c, double v, int p, int a){
+        this.name = n;
+        this.rotation = r;
+        this.colors = c;
+        this.value = v;
+        this.placed = p;
+        this.amount = a;
+    }
+
+
+public String getName(){
+    return this.name;
+}
+
+public int getRotations(){
+    return this.rotation;
+}
+
+public int getColor(){
+    return this.colors;
+}
+
+public double getValue(){
+    return this.value;
+}
+
+public int getPlaced(){
+    return this.placed;
+}
+
+public int getAmount(){
+    return this.amount;
+}
+
+public String setName(String n){
+    return this.name = n;
+}
+
+public int setRotations(int r){
+    return this.rotation = r;
+}
+
+public int setColor(int c){
+    return this.colors = c;
+}
+
+public double setValue(double v){
+    return this.value = v;
+}
+
+public int setPlaced(int p){
+    return this.placed = p;
+}
+
+public int setAmount(int a){
+    return this.amount = a;
+}
+
+
+
+
+    }
