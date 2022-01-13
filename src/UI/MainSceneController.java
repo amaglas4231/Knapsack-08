9d2aa6b329f2e48b9a890f65f8dc0010a3ae8fc5
Gunes <gunes.ozmen02@gmail.com>
2022-01-13 13:12:39 +0300

    UI

--- src/UI/MainSceneController.java
+++ src/UI/MainSceneController.java
@@ -3,25 +3,20 @@ package UI;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.geometry.Point3D;
 import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
-import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
+import Implementation.*;
 
-
+// alt shift F to format
 
 public class MainSceneController extends Application {
 
@@ -37,7 +32,6 @@ public class MainSceneController extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         SmartGroup group = new SmartGroup();
-       
 
         PhongMaterial red = new PhongMaterial();
         red.setDiffuseColor(Color.RED);
@@ -50,23 +44,36 @@ public class MainSceneController extends Application {
 
         PhongMaterial white = new PhongMaterial();
         white.setDiffuseColor(Color.WHITE);
-        
-        // int [][][] result = GreedyAlg.result;
+
+        int[][][] result = GreedyAlg.result;
 
         int xIndex = 0;
-        for(int ii = 0; ii < 5; ii++){
-            
+        for (int i = 0; i < 5; i++) {
+
             int yIndex = 0;
 
-            for(int jj = 0; jj < 8; jj++){
-                
+            for (int j = 0; j < 8; j++) {
+
                 int zIndex = 0;
-                
-                for(int k = 0; k < 33; k++){
 
-                    Box box = new Box(5,5,5);
+                for (int k = 0; k < 33; k++) {
 
-                    box.setMaterial(blue);
+                    Box box = new Box(5, 5, 5);
+
+                    if (result[i][j][k] == 0) {
+                        box.setMaterial(white);
+                    }
+                    if (result[i][j][k] == 3) {
+                        box.setMaterial(blue);
+
+                    } else if (result[i][j][k] == 4) {
+                        box.setMaterial(green);
+
+                    } else if (result[i][j][k] == 5) {
+                        box.setMaterial(red);
+
+                    }
+
                     box.translateZProperty().set(zIndex);
                     box.translateYProperty().set(yIndex);
                     box.translateXProperty().set(xIndex);
@@ -84,7 +91,6 @@ public class MainSceneController extends Application {
         Scene scene = new Scene(group, WIDTH, HEIGHT);
         scene.setFill(Color.SILVER);
         scene.setCamera(camera);
-        
 
         group.translateXProperty().set(WIDTH / 2);
         group.translateYProperty().set(HEIGHT / 2);
@@ -94,61 +100,61 @@ public class MainSceneController extends Application {
 
         primaryStage.setTitle("Trial");
         primaryStage.setScene(scene);
-        primaryStage.show();     
-        
+        primaryStage.show();
+
     }
 
-   private void initMouseControl(SmartGroup group, Scene scene, Stage stage) {
-       Rotate xRotate;
-       Rotate yRotate;
-       group.getTransforms().addAll(
+    private void initMouseControl(SmartGroup group, Scene scene, Stage stage) {
+        Rotate xRotate;
+        Rotate yRotate;
+        group.getTransforms().addAll(
                 xRotate = new Rotate(0, Rotate.X_AXIS),
-                yRotate = new Rotate(0, Rotate.Y_AXIS)
-        );
+                yRotate = new Rotate(0, Rotate.Y_AXIS));
         xRotate.angleProperty().bind(angleX);
         yRotate.angleProperty().bind(angleY);
 
-        scene.setOnMousePressed(event ->{
+        scene.setOnMousePressed(event -> {
             anchorX = event.getSceneX();
             anchorY = event.getSceneY();
             anchorAngleX = angleX.get();
             anchorAngleY = angleY.get();
         });
 
-        scene.setOnMouseDragged(event ->{
+        scene.setOnMouseDragged(event -> {
             angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
             angleY.set(anchorAngleY + anchorX - event.getSceneX());
         });
 
-        stage.addEventHandler(ScrollEvent.SCROLL, event ->{
+        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
             double movement = event.getDeltaY();
             group.translateZProperty().set(group.getTranslateZ() - movement);
         });
     }
 
-public static void main(String[] args) {
-       launch(args);
-        
-   }
-   class SmartGroup extends Group{
+    public static void main(String[] args) {
+        launch(args);
 
-    Rotate r;
-    Transform t = new Rotate();
+    }
 
-    void rotateByX (int ang){
-        r = new Rotate(ang, Rotate.X_AXIS);
-        t = t.createConcatenation(r);
-        this.getTransforms().clear();
-        this.getTransforms().addAll(t);
+    class SmartGroup extends Group {
 
-      }
+        Rotate r;
+        Transform t = new Rotate();
 
-      void rotateByY (int ang){
-        r = new Rotate(ang, Rotate.Y_AXIS);
-        t = t.createConcatenation(r);
-        this.getTransforms().clear();
-        this.getTransforms().addAll(t);
+        void rotateByX(int ang) {
+            r = new Rotate(ang, Rotate.X_AXIS);
+            t = t.createConcatenation(r);
+            this.getTransforms().clear();
+            this.getTransforms().addAll(t);
 
-      }
-   }
+        }
+
+        void rotateByY(int ang) {
+            r = new Rotate(ang, Rotate.Y_AXIS);
+            t = t.createConcatenation(r);
+            this.getTransforms().clear();
+            this.getTransforms().addAll(t);
+
+        }
+    }
 }
